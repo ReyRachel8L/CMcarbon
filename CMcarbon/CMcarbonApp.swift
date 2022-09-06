8d43ef3b09fc7bc71ec711b0d1da3b53b9fc7aa4
ReyRachel8L <rlim.rachel08@gmail.com>
2022-09-06 11:32:23 +0800

    Initial Commit

---
+++ CMcarbon/CMcarbonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMcarbonApp.swift
+//  CMcarbon
+//
+//  Created by LIM YEE SIN RACHEL stu on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct CMcarbonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
